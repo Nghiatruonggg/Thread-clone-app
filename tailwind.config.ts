997cd02137726a74d9bd0391c3f61131a09f64db
Nghiatruonggg <nghiatruong110700@gmail.com>
2025-01-18 16:32:39 +0700

    feat: adding icons and sidebar components

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,21 @@
 import type { Config } from "tailwindcss";
 
 export default {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--back-color)",
-        text: "var(--text-color)",
-        input: "var(--inputback-color)",
-        outline: "var(--outline-color)"
-      },
+    content: [
+        "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                background: "var(--back-color)",
+                text: "var(--text-color)",
+                input: "var(--inputback-color)",
+                outline: "var(--outline-color)",
+                navHover: "var(--nav-hover)",
+            },
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 } satisfies Config;
